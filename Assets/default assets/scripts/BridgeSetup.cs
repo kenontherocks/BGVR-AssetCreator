be9bef20e9b2655cea74067ee5d2e07603e927a1
Border7Ken <ken@border7.com>
2020-09-09 00:40:00 -0700

    added untracked files

---
+++ Assets/default assets/scripts/BridgeSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeSetup : MonoBehaviour
+{
+    public GameObject playerPlaceholder;
+    public GameObject cabinBody;
+
+    [Header("Crew Positions")]
+    public GameObject crewHelmPlaceholder;
+    public GameObject crewComsPlaceholder;
+    public GameObject crewTactPlaceholder;
+
+    public GameObject[] crewExtraPlaceholders;
+
+    [Header("Screen Positions")]
+    public GameObject shipAlert;
+    public GameObject shipStats;
+    public GameObject shipSpeed;
+
+}
